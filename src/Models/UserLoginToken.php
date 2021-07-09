ae081a8236bb2e1c3d14fc52a4a67bdd38f5a7c0
Kevin Oosterveen <kevinoosterveen@gmail.com>
2021-07-09 20:46:37 +0200

    Remove HasFactory trait from UserLogin model.

--- src/Models/UserLoginToken.php
+++ src/Models/UserLoginToken.php
@@ -3,18 +3,9 @@
 
 namespace Koost89\UserLogin\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Koost89\UserLogin\Database\Factories\UserLoginTokenFactory;
 
 class UserLoginToken extends Model
 {
-    use HasFactory;
-
     protected $fillable = ['url'];
-
-    protected static function newFactory(): UserLoginTokenFactory
-    {
-        return UserLoginTokenFactory::new();
-    }
 }
