526d21b6df71a64c59281324659c57919b3a58b2
Kevin Oosterveen <kevinoosterveen@gmail.com>
2021-07-11 19:44:38 +0200

    Updated testbench.yml

--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -2,14 +2,14 @@
 
 namespace Koost89\LoginLinks\Tests;
 
-use Koost89\LoginLinks\LoginLink;
+use Koost89\LoginLinks\Facades\LoginLink;
 use Koost89\LoginLinks\Tests\TestClasses\User;
 
 class UrlTest extends TestCase
 {
     public function test_it_generates_a_signed_url()
     {
-        $url = (new LoginLink())->generate(User::first());
+        $url = LoginLink::generate(User::first());
 
         $this->assertStringContainsString(config('login-links.route.path'), $url);
         $this->assertStringContainsString("auth_id=1", $url);
@@ -25,7 +25,7 @@ class UrlTest extends TestCase
             ->where('id', '!=', $user->id)
             ->first();
 
-        $url = (new LoginLink())->generate($user);
+        $url = LoginLink::generate($user);
 
         $badUrl = str_replace('auth_id=' . $user->id, 'auth_id=' . $otherUser->id, $url);
 
